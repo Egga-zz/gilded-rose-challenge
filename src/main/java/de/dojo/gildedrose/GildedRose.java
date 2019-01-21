e5f6c8a575465dbe42f6db8f867ddd3921e30512
Egga <github@egga.de>
2019-01-21 11:21:02 +0100

    indent

--- src/main/java/de/dojo/gildedrose/GildedRose.java
+++ src/main/java/de/dojo/gildedrose/GildedRose.java
@@ -16,24 +16,24 @@ class GildedRose {
     private void updateItem(Item item) {
         if (item.name.equals("Aged Brie")
                 || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
 
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
-                } else {
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            }
+        } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.quality = item.quality - 1;
