291975ff22f8afa4377b6160be0453a44993b733
Egga <github@egga.de>
2019-01-21 11:34:34 +0100

    duplicate method

--- src/main/java/de/dojo/gildedrose/GildedRose.java
+++ src/main/java/de/dojo/gildedrose/GildedRose.java
@@ -9,11 +9,57 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItem(item);
+
+            if (isAgedBrie(item)) {
+                updateAgedBrie(item);
+            } else {
+                updateItem(item);
+            }
         }
     }
 
     private void updateItem(Item item) {
+        if (isAgedBrie(item) || isBackstagePasses(item)) {
+            if (isBelowMaxQuality(item)) {
+                incrementQuality(item);
+
+                if (isBackstagePasses(item)) {
+                    if (item.sellIn < 11 && isBelowMaxQuality(item)) {
+                        incrementQuality(item);
+                    }
+
+                    if (item.sellIn < 6 && isBelowMaxQuality(item)) {
+                        incrementQuality(item);
+                    }
+                }
+            }
+        } else if (isAboveMinQuality(item) && !isSulfuras(item)) {
+            decrementQuality(item);
+        }
+
+        if (!isSulfuras(item)) {
+            decrementSellIn(item);
+        }
+
+        if (item.sellIn < 0) {
+            if (isAgedBrie(item)) {
+                if (isBelowMaxQuality(item)) {
+                    incrementQuality(item);
+                }
+            } else {
+                if (isBackstagePasses(item)) {
+                    item.quality = 0;
+                } else {
+                    if (isAboveMinQuality(item) && !isSulfuras(item)) {
+                        decrementQuality(item);
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void updateAgedBrie(Item item) {
         if (isAgedBrie(item) || isBackstagePasses(item)) {
             if (isBelowMaxQuality(item)) {
                 incrementQuality(item);
