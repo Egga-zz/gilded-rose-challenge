21f6e148a6d4a6ef85759c5fd2251e2eb538ba75
Egga <github@egga.de>
2019-01-21 10:48:51 +0100

    remove unnecessary clutter

--- src/test/java/de/dojo/gildedrose/GildedRoseTest.java
+++ src/test/java/de/dojo/gildedrose/GildedRoseTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class GildedRoseTest {
+class GildedRoseTest {
 
     @Test
-    public void items_degrade_twice_as_fast_after_expire_date() {
+    void items_degrade_twice_as_fast_after_expire_date() {
         // given
         Item firstItem = new Item("Philipp", -1, 5);
         GildedRose gildedRose = new GildedRose(new Item[]{firstItem});
@@ -23,8 +23,8 @@ public class GildedRoseTest {
         assertThat(initialQuality - secondQuality).isEqualTo(2);
     }
 
-    // @Test
-    public void items_degrade_in_quality_over_time() {
+    @Test
+    void items_degrade_in_quality_over_time() {
         // when
         Item firstItem = updateQualityForSingleItem("Philipp", 1, 5);
 
@@ -32,8 +32,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(4);
     }
 
-    // @Test
-    public void items_never_have_negative_quality() {
+    @Test
+    void items_never_have_negative_quality() {
         // when
         Item firstItem = updateQualityForSingleItem("Philipp", 1, 0);
 
@@ -41,8 +41,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isGreaterThanOrEqualTo(0);
     }
 
-    // @Test
-    public void sellin_degrades_over_time() {
+    @Test
+    void sellin_degrades_over_time() {
         // when
         Item firstItem = updateQualityForSingleItem("Philipp", 3, 5);
 
@@ -50,8 +50,8 @@ public class GildedRoseTest {
         assertThat(firstItem.sellIn).isEqualTo(2);
     }
 
-    // @Test
-    public void aged_brie_quality_increases_over_time() {
+    @Test
+    void aged_brie_quality_increases_over_time() {
         // when
         Item firstItem = updateQualityForSingleItem("Aged Brie", 1, 5);
 
@@ -59,8 +59,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(6);
     }
 
-    // @Test
-    public void quality_of_an_item_is_never_above_50() {
+    @Test
+    void quality_of_an_item_is_never_above_50() {
         // when
         Item firstItem = updateQualityForSingleItem("Aged Brie", 1, 50);
 
@@ -68,8 +68,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(50);
     }
 
-    // @Test
-    public void sulfuras_never_expires() {
+    @Test
+    void sulfuras_never_expires() {
         // when
         Item firstItem = updateQualityForSingleItem("Sulfuras, Hand of Ragnaros", 1, 80);
 
@@ -77,8 +77,8 @@ public class GildedRoseTest {
         assertThat(firstItem.sellIn).isEqualTo(1);
     }
 
-    // @Test
-    public void sulfuras_has_a_stable_quality() {
+    @Test
+    void sulfuras_has_a_stable_quality() {
         // when
         Item firstItem = updateQualityForSingleItem("Sulfuras, Hand of Ragnaros", 1, 80);
 
@@ -86,8 +86,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(80);
     }
 
-    // @Test
-    public void backstage_pass_quality_increases_by_one_if_sellin_is_equal_to_eleven() {
+    @Test
+    void backstage_pass_quality_increases_by_one_if_sellin_is_equal_to_eleven() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 11, 5);
 
@@ -95,8 +95,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(6);
     }
 
-    // @Test
-    public void backstage_pass_quality_increases_by_two_if_sellin_is_equal_to_ten() {
+    @Test
+    void backstage_pass_quality_increases_by_two_if_sellin_is_equal_to_ten() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 10, 5);
 
@@ -104,8 +104,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(7);
     }
 
-    // @Test
-    public void backstage_pass_quality_increases_by_two_if_sellin_is_equal_to_six() {
+    @Test
+    void backstage_pass_quality_increases_by_two_if_sellin_is_equal_to_six() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 6, 5);
 
@@ -113,8 +113,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(7);
     }
 
-    // @Test
-    public void backstage_pass_quality_increases_by_three_if_sellin_is_equal_to_five() {
+    @Test
+    void backstage_pass_quality_increases_by_three_if_sellin_is_equal_to_five() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 5, 5);
 
@@ -122,8 +122,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(8);
     }
 
-    // @Test
-    public void backstage_pass_quality_increases_by_three_if_sellin_is_today() {
+    @Test
+    void backstage_pass_quality_increases_by_three_if_sellin_is_today() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 1, 5);
 
@@ -131,8 +131,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(8);
     }
 
-    // @Test
-    public void backstage_pass_quality_drops_to_zero_after_sellin() {
+    @Test
+    void backstage_pass_quality_drops_to_zero_after_sellin() {
         // when
         Item firstItem = updateQualityForSingleItem("Backstage passes to a TAFKAL80ETC concert", 0, 5);
 
@@ -140,8 +140,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(0);
     }
 
-    // @Test
-    public void aged_brie_does_not_surpass_quality_of_fifty_when_expired() {
+    @Test
+    void aged_brie_does_not_surpass_quality_of_fifty_when_expired() {
         // when
         Item firstItem = updateQualityForSingleItem("Aged Brie", -1, 50);
 
@@ -149,8 +149,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(50);
     }
 
-    // @Test
-    public void aged_brie_increases_quality_by_one_when_unfinished() {
+    @Test
+    void aged_brie_increases_quality_by_one_when_unfinished() {
         // when
         Item firstItem = updateQualityForSingleItem("Aged Brie", 1, 1);
 
@@ -158,8 +158,8 @@ public class GildedRoseTest {
         assertThat(firstItem.quality).isEqualTo(2);
     }
 
-    // @Test
-    public void aged_brie_increases_quality_by_two_when_expired() {
+    @Test
+    void aged_brie_increases_quality_by_two_when_expired() {
         // when
         Item firstItem = updateQualityForSingleItem("Aged Brie", -1, 48);
 
