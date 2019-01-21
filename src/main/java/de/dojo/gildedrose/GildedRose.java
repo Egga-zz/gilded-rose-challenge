b4392f95fe0c3222a03e036a38f4904d40b8b970
Egga <github@egga.de>
2019-01-21 11:20:11 +0100

    invert ifs

--- src/main/java/de/dojo/gildedrose/GildedRose.java
+++ src/main/java/de/dojo/gildedrose/GildedRose.java
@@ -14,29 +14,29 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (item.name.equals("Aged Brie")
+                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (item.quality < 50) {
+                                    item.quality = item.quality + 1;
+                                }
+                            }
+                        }
+                    }
+                } else {
             if (item.quality > 0) {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                     item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
                 }
             }
         }
@@ -46,19 +46,19 @@ class GildedRose {
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else {
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
+                } else {
                     if (item.quality > 0) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             item.quality = item.quality - 1;
                         }
                     }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
                 }
             }
         }
